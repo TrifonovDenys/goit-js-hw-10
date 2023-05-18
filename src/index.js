0d0accceb0cd8d7e5f4647704d934a50f5bdf032
Trifonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-05-18 17:21:51 +0300

    not hw

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+import pokemonCardTpl from '../src/templ/pokemon-card.hbs'
+const a = document.querySelector('.js-card')
+const serchInput = document.querySelector('#search-box')
+
+
+function fetchPokemon(pokemonId) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(response => {
+    return response.json()
+  })
+}
+
+function renderPokemon(pokemon) {
+  const markup = pokemonCardTpl(pokemon) 
+  a.innerHTML = markup
+}
+
+serchInput.addEventListener('input', debounce(inSerch, 0))
+
+function inSerch(e) {
+  e.preventDefault()
+  console.log(e.target.value);
+  fetchPokemon(e.target.value)
+  .then(renderPokemon)
+  .catch(error => {
+  console.log(error);
+})
+}
